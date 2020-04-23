0f503146cdd5ff70e65d95388ae98d2f30cd997c
Przemysław Rychter <39413606+PrzemekRychter@users.noreply.github.com>
2020-04-24 00:50:46 +0200

    Update generateData.m
    DO USUNIECIA

--- Symulator/generateData.m
+++ Symulator/generateData.m
@@ -1,4 +1,5 @@
-%Funkcja generujaca  ci?gów binarnych o d?ugo?ci length
+%Funkcja generujaca  ci?gÃ³w binarnych o d?ugo?ci length
 function data = generateData(length)
 data = randi([0 1],length,1);
 end
+
