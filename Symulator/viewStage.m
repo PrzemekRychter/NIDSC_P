68355d8ce791be17677571f2182e2b08fd87bf76
Przemysław Rychter <39413606+PrzemekRychter@users.noreply.github.com>
2020-04-24 01:00:53 +0200

    Add files via upload
    Funkcja do wyświetlania postępu podczas symulacji

---
+++ Symulator/viewStage.m
@@ -0,0 +1,21 @@
+function  staryProcent = viewStage(staryProcent,u,ileRazy)
+      nie = 0;
+      procent = idivide((u/ileRazy)*100,int16(1));
+      if procent == staryProcent
+        nie = 1;
+      end
+      staryProcent = procent;
+      if (procent < 100 && nie == 0)
+          if u ~= 1
+              fprintf("\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b");
+              if procent < 11
+                fprintf("\b");
+              else 
+                fprintf("\b\b");
+              end
+          end
+          fprintf("\t %d%% of simulation completed.",procent)
+      end
+end
+
+
