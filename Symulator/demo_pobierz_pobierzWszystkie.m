3c1a25fbbcbec9ee0f40e985448cb5f058ee90b5
Przemysław Rychter <39413606+PrzemekRychter@users.noreply.github.com>
2020-04-24 15:16:37 +0200

    Update and rename pobranieWykresu.m to demo_pobierz_pobierzWszystkie.m

---
+++ Symulator/demo_pobierz_pobierzWszystkie.m
@@ -0,0 +1,16 @@
+% Pobranie n-tych danych z okno.fig i wyświetlenie ich
+% w oknie 10, w folderze musi się znajdować okno.fig
+n = 2;
+[xy,col] = pobierz("okno.fig",n);
+figure(10);
+plot(xy(1,:),xy(2,:),'color',col);
+
+% pobranie wszystkich danych z okno.fig
+% wyswielenie ich w oknie 20
+
+[X,Y] = pobierzWszystkie("okno.fig");
+for n = 1 : size(X,1)
+    figure(20)
+    hold on
+    plot(X(n,:),Y(n,:));
+end
--- Symulator/pobranieWykresu.m
+++
@@ -1,8 +0,0 @@
-% Pobranie n-tych danych z okno.fig i wyświetlenie ich
-% w folderze musi się znajdować okno.fig
-
-n = 2;
-[xy,col] = pobierz("okno.fig",n);
-figure();
-plot(xy(1,:),xy(2,:),'color',col);
-
