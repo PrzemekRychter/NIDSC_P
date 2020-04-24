4dd9f6c620336bf8b22367d18243abdb818e7a91
Przemysław Rychter <39413606+PrzemekRychter@users.noreply.github.com>
2020-04-24 15:04:44 +0200

    Update pobierz.m
    Zwraca n-ty wykres oraz jego kolor

--- Symulator/pobierz.m
+++ Symulator/pobierz.m
@@ -1,13 +1,26 @@
-% Funkcja pobiera n-ty wykres z okna xyz.fig
-% zapisuje x w 1 wierszu xy, y w 2 wierszu xy
+% Funkcja pobiera n-ty wykres z okna nazwaFig ('nazwa.fig')
+% zapisuje x w 1 wierszu xy, y w 2 wierszu xy. 
+% w wektorze col jest zapisany kolor RGB
+%
 
-function xy  = pobierz(n)
-    open xyz.fig % Otwiera obiekt fig o nazwie okno
+function [xy,col]  = pobierz(nazwaFig,n)
+    openfig(nazwaFig) % Otwiera obiekt fig o nazwie okno
     D=get(gca,'Children'); 
     XData=get(D,'XData');       %XData ma komórki typu cell
     YData=get(D,'YData'); 
     rozmiar = size(XData,1);
+    ax =gca;
+    colors = ax.ColorOrder ;
+    
+    for index  = ax.ColorOrderIndex : n     
+        n_colors = size(colors,1);
+        if index > n_colors
+            n = 1;
+        end
+        nextC = colors(index,:);
+    end
     xy(1,:) = XData{rozmiar +1 - n};
     xy(2,:) = YData{rozmiar +1 -n};
+    col = nextC;
 end
 
