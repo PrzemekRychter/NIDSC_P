56d5c85a4a69caec2d4c9f7fc95f7a56323c077e
Przemysław Rychter <39413606+PrzemekRychter@users.noreply.github.com>
2020-04-24 15:06:12 +0200

    Update pobranieWykresu.m

--- Symulator/pobranieWykresu.m
+++ Symulator/pobranieWykresu.m
@@ -1,7 +1,8 @@
-% Pobranie n-tych danych z okna xyz.fig i wyświetlenie ich
-% w folderze musi się znajdować okno xyz.fig
+% Pobranie n-tych danych z okno.fig i wyświetlenie ich
+% w folderze musi się znajdować okno.fig
 
-n = 1;
-xy = pobierz(n);
+n = 2;
+[xy,col] = pobierz("okno.fig",n);
 figure();
-plot(xy(1,:),xy(2,:));
+plot(xy(1,:),xy(2,:),'color',col);
+
